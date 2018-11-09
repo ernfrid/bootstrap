064521e0110610422f80fe2603229724d94e68a5
Dave Larson <delarson@wustl.edu>
2018-11-09 16:23:04 +0000

    Add support for optional dot files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,19 @@
 
 set -eo pipefail
 
+option="$1"
+
 repo=$(dirname $0)
 echo $repo
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -r ${repo}/dotfiles/.[^.]* ~/
+if [[ ! -z ${option} ]]; then
+    # Overwrite the dotfiles
+    for s in $(ls ${repo}/dotfiles/.[^.]*-${option}); do
+        d=$(echo "$s" | sed "s/-${option}\$//")
+        echo "$s"
+        echo "$d"
+        cp -f "$s" "$d"
+    done
+fi
 vim +PluginInstall +qall
